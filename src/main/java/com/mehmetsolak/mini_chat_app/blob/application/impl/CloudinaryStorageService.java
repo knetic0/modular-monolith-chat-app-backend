70b84735d9f4cd5323ffbfca969f593e98acb42e
mehmet.solak <solakmehmet02@gmail.com>
2026-08-22 17:36:27 +0300

    (fix): remove bean name on cloudinary storage service

--- src/main/java/com/mehmetsolak/mini_chat_app/blob/application/impl/CloudinaryStorageService.java
+++ src/main/java/com/mehmetsolak/mini_chat_app/blob/application/impl/CloudinaryStorageService.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 
-@Service("cloudinary")
+@Service
 @RequiredArgsConstructor
 public class CloudinaryStorageService implements StorageService {
 
